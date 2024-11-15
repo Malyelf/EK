1cb74cdcb17117c0b3c0203414d7bbb61ddf376c
Your Name <you@example.com>
2024-11-15 15:14:38 +0100

    Bearbeitung 1

---
+++ about.php
@@ -0,0 +1,31 @@
+
+<?php require_once('./assets/layout/header.php'); ?>
+
+<!-- About Section -->
+    <section id="about" class="about section">
+
+    <div class="container">
+
+        <div class="row position-relative">
+
+            <div class="col-lg-7 about-img" data-aos="zoom-out" data-aos-delay="00"><img src="assets/img/bild.jpeg"></div>
+
+                <div class="col-lg-7" data-aos="fade-up" data-aos-delay="100">
+                    <h2 class="inner-title">Wer bin Ich</h2>
+                     <div class="our-story">
+                              <h1>Martin Kempa</h1>
+                                <p>Wohnhaft in Essen, beruflich befinde mich gerade in einer Umschulung zum Fachinformatiker in Daa Obrehausen und hoffe bald endlich fertig zu werden.</p>
+
+                    
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section><!-- /About Section -->
+   
+
+<?php require_once('./assets/layout/footer.php'); ?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php require_once('./assets/layout/header.php'); ?>
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section dark-background">
+
+      <div id="hero-carousel" data-bs-interval="5000" class="container carousel carousel-fade" data-bs-ride="carousel">
+
+          <!-- Slide 1 -->
+           
+
+          
+            <div class="carousel-container">
+              <h2 class="animate__animated animate__fadeInDown">Herzlich wilkommen <span>auf meiner persönlichen Webseite!</span></h2>
+              <p class="animate__animated animate__fadeInUp">Kurs: Webprogramierung by Martin Kempa</p>
+              <a href="./kontakt.php" class="btn-get-started animate__animated animate__fadeInUp scrollto">Kontaktiere mich</a>
+
+            </div>
+        
+        
+      </div>
+
+    </section><!-- /Hero Section -->
+
+<?php require_once('./assets/layout/footer.php'); ?>
+ 
